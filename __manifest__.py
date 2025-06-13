4c45957e82f718de0e610e445c7d7c946e2224f5
Breithner <Breithnerap@gmail.com>
2025-06-13 14:06:34 -0500

    upload change

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Scraping de Ofertas de trabajo',
+    'version': '',
+    'summary': """ Módulo que trae ofertas desde portales de trabajos """,
+    'author': 'Breithner Aquituari',
+    'website': '',
+    'category': '',
+    'depends': ['base', ],
+    'data': [
+        
+    ],
+    
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
